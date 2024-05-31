38ed93c7d8861d73d0ccdeb9383b0b5a8c968af7
liangmadaohang <unclezhou486@outlook.com>
2024-05-31 14:09:49 +0800

    Site updated: 2024-05-31 14:09:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/30/hello-world/"];function toRandomPost(){
+var posts=["2024/05/30/hello-world/","2024/05/31/建站完成并且初步配置完成/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
